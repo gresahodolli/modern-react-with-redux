bbd19a02935deba469d49879d695fb94874922da
Gresa Hodolli <gresa.hodolli1@student.uni-pr.edu>
2023-11-15 13:34:14 +0100

    Tectarea

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,7 +10,17 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-    return <h1>{new Date().toLocaleTimeString()}</h1>;
+    return (
+        <div className='wrapper'>
+            <textarea 
+                readOnly
+                maxLength={3}
+                spellCheck
+                style={{backgroundColor:'gray'}}
+            />
+
+        </div>
+    )
 }
 
 
