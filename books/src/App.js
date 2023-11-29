1daaf54bd90b33b9345aa4bfd88d9e80675d270d
gresahodolli <94695584+gresahodolli@users.noreply.github.com>
2023-11-29 10:49:57 +0100

    Creating a new record

--- books/src/App.js
+++ books/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
@@ -25,10 +26,16 @@ function App() {
         setBooks(updatedBooks);
     }
 
-    const createBook = (title) => { 
+    const createBook = async (title) => { 
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+
+
+
         const updatedBooks = [
             ...books,
-            { id: Math.round(Math.random() * 999), title },
+            response.data
         ];
         setBooks(updatedBooks);
     };
