522004a1e752a268762f7207ce48ba2791144637
gresahodolli <94695584+gresahodolli@users.noreply.github.com>
2023-11-29 16:43:18 +0100

    Variation on Event Handlers
    something is wrong

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -4,16 +4,17 @@ import { useState } from 'react';
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
-
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+  
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    const content = isExpanded && <div>{item.content}</div>
-
     return (
       <div key={item.id}> 
-        <div>{item.label}</div>
-        {content}
+        <div onClick={()=> handleClick(index)}></div>
+       {isExpanded && <div> {item.content}</div>}
       </div>
     )
   });
