ce9933299af28aa0c10e5eca131028ccc89e53a3
gresahodolli <94695584+gresahodolli@users.noreply.github.com>
2023-12-02 22:17:31 +0100

    Navigation Context

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,28 +1,9 @@
-import React, { useState } from 'react'
-import Dropdown from './components/Dropdown';
-import { ClassName } from 'classnames';
+import React from 'react'
 
 function App() {
-    const [selection, setSelection] = useState(null);
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    }
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', vlaue: 'blue' },
-    ];
-
-
   return (
-    <div className='flex'>
-        <a href="/dashboard">Dashboard</a>
-        <Dropdown options={options} value={selection} onChange={handleSelect} />
-        <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
+    <div>App</div>
   )
 }
 
-export default App
+export default App 
