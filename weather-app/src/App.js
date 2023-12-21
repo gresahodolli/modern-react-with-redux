1faf5a17c8bb7ade7be20195f2202224198f9dc0
gresahodolli <94695584+gresahodolli@users.noreply.github.com>
2023-12-21 22:32:18 +0100

    Update

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import hotBg from "./assets/hotBg.jpg";
+import coldBg from "./assets/coldBg.jpg";
+import './style.css';
+
 
 function App() {
   return (
-    <div>App</div>
+    <div className='app' style={{bacgroundImage: `url(${coldBg})`}}>
+      <div className='overlay'>
+        <div className='container'>
+          <div className='section section__inputs'>
+            <input type="text" name="city" placeholder="Enter City.
+            .." />
+            <button>F</button>
+          </div>
+          
+          <div className='section section_temperature'>
+            <div className='icon'>
+              <h3>London, GB</h3>
+              <img 
+                src="https://openweathermap.org/img/wn/02@2x.png" 
+                alt="weatherIcon"
+              />
+              <h3>Cloudy</h3>
+            </div>
+            <div className='temperature'>
+              <h1>34 C</h1>
+            </div>
+          </div>
+
+          {/* bottom description  */}
+        </div>
+      </div>
+    </div>
   )
 }
 
