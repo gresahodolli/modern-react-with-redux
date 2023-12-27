79c4908466ae42f876f658f17711af0c079262b8
gresahodolli <94695584+gresahodolli@users.noreply.github.com>
2023-12-27 16:24:03 +0100

    without api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,37 +1,11 @@
-import React from 'react';
-import hotBg from "./assets/hotBg.jpg";
-import coldBg from "./assets/coldBg.jpg";
-import './style.css';
-
+import './App.css';
+import WeatherApp from './components/WeatherApp/WeatherApp';
 function App() {
   return (
-    <div className='app' style={{backgroundImage: `url(${coldBg})`}}>
-      <div className='overlay'>
-        <div className='container'>
-          <div className='section section__inputs'>
-            <input type="text" name="city" placeholder="Enter City..." />
-            <button>F</button>
-          </div>
-          
-          <div className='section section_temperature'>
-            <div className='icon'>
-              <h3>London, GB</h3>
-              <img 
-                src="https://freepngimg.com/thumb/categories/2275.png" 
-                alt="weatherIcon"
-              />
-              <h3>Cloudy</h3>
-            </div>
-            <div className='temperature'>
-              <h1>34 C</h1>
-            </div>
-          </div>
-
-          {/* bottom description  */}
-        </div>
-      </div>
+    <div className="App">
+     <WeatherApp />
     </div>
-  )
+  );
 }
 
 export default App;
